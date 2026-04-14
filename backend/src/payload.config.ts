c2500e67bb823e346e1b078328b17b018cf1986a
chronic-int <chronicislegend.com>
2026-04-14 17:55:43 +0200

    feat: initialize backend project with Payload CMS configuration and dependencies

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -1,4 +1,4 @@
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -37,10 +37,8 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URL || 'file:./backend.db',
-    },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URL || 'mongodb://127.0.0.1/oaworkshop',
   }),
   sharp,
   cors: [
